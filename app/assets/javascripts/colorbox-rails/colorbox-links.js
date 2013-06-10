f2bebea082ba8b4c84b67799ab53cc6d0b92b1df
Torsten Trautwein <trautwein@me.com>
2013-06-10 16:47:36 +0200

    Update colorbox-links.js
    support for "colorbox-rel" data attribute

--- app/assets/javascripts/colorbox-rails/colorbox-links.js
+++ app/assets/javascripts/colorbox-rails/colorbox-links.js
@@ -29,6 +29,7 @@ $(document).ready(function() {
                 className: $(this).data("colorbox-class-name") || false,
                 href: $(this).data("colorbox-href") || $(this).attr('href'),
                 inline: $(this).data("colorbox-inline") || false,
+                rel: $(this).data("colorbox-rel") || false,
                 opacity: 0.5
             });
     });
@@ -46,7 +47,8 @@ $(document).ready(function() {
                 title: function() { return $(this).data("colorbox-title") || false },
                 className: function() { return $(this).data("colorbox-class-name") || false },
                 href: function() { return $(this).data("colorbox-href") || $(this).attr('href') },
-                inline: function() { return $(this).data("colorbox-inline") || false},
+                inline: function() { return $(this).data("colorbox-inline") || false },
+                rel: function() { return $(this).data("colorbox-rel") || false },
                 opacity: 0.5
             });
 });
