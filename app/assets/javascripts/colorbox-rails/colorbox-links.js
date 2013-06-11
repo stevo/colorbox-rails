8d75f05f4746589fb3df040f62862c1840da8bc8
Torsten Trautwein <trautwein@me.com>
2013-06-11 17:09:43 +0300

    Update colorbox-links.js
    testing grouping..

--- app/assets/javascripts/colorbox-rails/colorbox-links.js
+++ app/assets/javascripts/colorbox-rails/colorbox-links.js
@@ -9,27 +9,34 @@
 (function($){
 
 $(document).ready(function() {
-	
-    $(document).on('click', '[data-colorbox="true"]', function(e) {
-        e.preventDefault();
-
-        $('[data-colorbox="true"]').colorbox(
-	    {
-	        height: function() { return $(this).data("colorbox-height") || false },
-	        width: function() { return $(this).data("colorbox-width") || false },
-	        maxWidth: function() { return $(this).data("colorbox-max-width") || false },
-	        maxHeight: function() { return $(this).data("colorbox-max-height") || false },
-	        iframe: function() { return $(this).data("colorbox-iframe") || false },
-	        photo: function() { return $(this).data("colorbox-photo") || false },
-	        innerHeight: function() { return $(this).data("colorbox-innerheight") || false },
-	        innerWidth: function() { return $(this).data("colorbox-innerwidth") || false },
-	        title: function() { return $(this).data("colorbox-title") || false },
-	        className: function() { return $(this).data("colorbox-class-name") || false },
-	        href: function() { return $(this).data("colorbox-href") || $(this).attr('href') },
-	        inline: function() { return $(this).data("colorbox-inline") || false },
-	        opacity: 0.5
-	    });
+  
+  function refreshColorboxLinks() {
+    $('[data-colorbox="true"]').colorbox({
+      height: function() { return $(this).data("colorbox-height") || false },
+      width: function() { return $(this).data("colorbox-width") || false },
+      maxWidth: function() { return $(this).data("colorbox-max-width") || false },
+      maxHeight: function() { return $(this).data("colorbox-max-height") || false },
+      iframe: function() { return $(this).data("colorbox-iframe") || false },
+      photo: function() { return $(this).data("colorbox-photo") || false },
+      innerHeight: function() { return $(this).data("colorbox-innerheight") || false },
+      innerWidth: function() { return $(this).data("colorbox-innerwidth") || false },
+      title: function() { return $(this).data("colorbox-title") || false },
+      className: function() { return $(this).data("colorbox-class-name") || false },
+      href: function() { return $(this).data("colorbox-href") || $(this).attr('href') },
+      inline: function() { return $(this).data("colorbox-inline") || false },
+      opacity: 0.5
     });
+  }
+
+  $(document).on('click', '[data-colorbox="true"]', function(e) {
+      e.preventDefault();
+
+      refreshColorboxLinks();
+  });
+
+  $(document).change(function() {
+    refreshColorboxLinks();
+  });
 });
 
 }) (jQuery);
